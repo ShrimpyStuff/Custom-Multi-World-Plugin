23f28bfa8ac43de6ad19c18b56690868b5b3c5e9
ElCholoGamer <josedanielgrayson@gmail.com>
2021-02-13 18:48:03 -0500

    update create command

--- src/main/java/ca/sajid/custommultiworldplugin/commands/CreateWorld.java
+++ src/main/java/ca/sajid/custommultiworldplugin/commands/CreateWorld.java
@@ -8,44 +8,54 @@ import org.bukkit.WorldType;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CreateWorld extends BaseCommand {
 
     public CreateWorld() {
         super("create");
     }
+
     @Override
     public boolean execute(CommandSender sender, Command cmd, String label, String[] args) {
-        if (args.length == 0) {
-            sender.sendMessage(Utils.color("&c Missing arguments [Name] [Normal|Nether|End] (AMPLIFIED|FLAT|LARGE_BIOMES)"));
-            return false;
-        }
+        if (args.length < 3) return false;
 
         WorldCreator wc = new WorldCreator(args[0]);
 
-        switch (args[1].toUpperCase()) {
-            case "NORMAL":
-                wc.environment(World.Environment.NORMAL);
-                break;
-            case "NETHER":
-                wc.environment(World.Environment.NETHER);
-                break;
-            case "END":
-                wc.environment(World.Environment.THE_END);
-                break;
+        try {
+            wc.environment(World.Environment.valueOf(args[1].toUpperCase()));
+        } catch (IllegalArgumentException e) {
+            sender.sendMessage(Utils.color("&cInvalid world enviroment!"));
+            return true;
         }
 
-        if (args.length < 3 || args[2].toUpperCase().equals("NORMAL")) {
-            wc.type(WorldType.NORMAL);
-        } else if (args[2].toUpperCase().equals("AMPLIFIED")) {
-            wc.type(WorldType.AMPLIFIED);
-        } else if (args[2].toUpperCase().equals("FLAT")) {
-            wc.type(WorldType.FLAT);
-        } else if (args[2].toUpperCase().equals("LARGE_BIOMES") || args[2].toUpperCase().equals("LARGE BIOMES")) {
-            wc.type(WorldType.LARGE_BIOMES);
+        try {
+            wc.type(WorldType.valueOf(args[2].toUpperCase()));
+        } catch (IllegalArgumentException e) {
+            sender.sendMessage(Utils.color("6cInvalid world type!"));
+            return true;
         }
 
         wc.createWorld();
-
         return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        List<String> options = new ArrayList<>();
+
+        if(args.length == 2) {
+            for (World.Environment env : World.Environment.values()) {
+                options.add(env.name().toLowerCase());
+            }
+        } else if (args.length == 3) {
+            for(WorldType type : WorldType.values()) {
+                options.add(type.name().toLowerCase());
+            }
+        }
+
+        options.removeIf(s -> !s.startsWith(args[args.length - 1]));
+        return options;
     }
 }
