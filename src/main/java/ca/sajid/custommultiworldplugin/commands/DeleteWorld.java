2890b0bdfdacca7caaa4907cb97f83537b61cb24
ShrimpyStuff <sajidmonowar11@gmail.com>
2021-02-14 15:35:45 -0500

    Lets hope this works

--- src/main/java/ca/sajid/custommultiworldplugin/commands/DeleteWorld.java
+++ src/main/java/ca/sajid/custommultiworldplugin/commands/DeleteWorld.java
@@ -1,7 +1,8 @@
 package ca.sajid.custommultiworldplugin.commands;
 
+import ca.sajid.custommultiworldplugin.CustomMultiWorldPlugin;
 import ca.sajid.custommultiworldplugin.util.BaseCommand;
-import org.bukkit.Bukkit;
+import org.bukkit.Server;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -32,18 +33,21 @@ public class DeleteWorld extends BaseCommand {
         }
     }
 
+    private static CustomMultiWorldPlugin plugin;
+    private static Server server = plugin.getServer();
+
     @Override
     public boolean execute(CommandSender sender, Command cmd, String label, String[] args) {
 
-        if (Bukkit.getWorld(args[0]) == null) {
+        if (server.getWorld(args[0]) == null) {
             return false;
         }
 
-        List<Player> players = Bukkit.getWorld(args[0]).getPlayers();
+        List<Player> players = server.getWorld(args[0]).getPlayers();
 
         if (!players.isEmpty()) {
             for (Player player : players) {
-                player.teleport(Bukkit.getWorld("Lobby").getSpawnLocation());
+                player.teleport(server.getWorld("Lobby").getSpawnLocation());
                 player.sendTitle("World" + args[0], "is getting deleted.", 5, 100, 5);
             }
         }
@@ -61,8 +65,8 @@ public class DeleteWorld extends BaseCommand {
             //I don't have anything to put here
         }
 
-        if (!Bukkit.getWorld(args[0]).getName().equals(levelName) || !Bukkit.getWorld(args[0]).getName().equals(levelName + "_nether") || !Bukkit.getWorld(args[0]).getName().equals(levelName + "_the_end")) {
-            Bukkit.unloadWorld(args[0], false);
+        if (!server.getWorld(args[0]).getName().equals(levelName) || !server.getWorld(args[0]).getName().equals(levelName + "_nether") || !server.getWorld(args[0]).getName().equals(levelName + "_the_end")) {
+            server.unloadWorld(args[0], false);
             deleteRecursively(new File(args[0]));
         }
 
@@ -74,7 +78,7 @@ public class DeleteWorld extends BaseCommand {
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
         List<String> options = new ArrayList<>();
 
-            List<World> worlds = Bukkit.getWorlds();
+            List<World> worlds = server.getWorlds();
 
 
         for (World world : worlds) {
