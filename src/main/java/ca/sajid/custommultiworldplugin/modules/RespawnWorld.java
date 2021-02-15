dfee1f13af49347f3c69d298a15df7249723ad93
ShrimpyStuff <52041627+ShrimpyStuff@users.noreply.github.com>
2021-02-14 20:52:21 -0500

    Update and rename onPlayerDeath.java to RespawnWorld.java

---
+++ src/main/java/ca/sajid/custommultiworldplugin/modules/RespawnWorld.java
@@ -0,0 +1,24 @@
+package ca.sajid.custommultiworldplugin.modules;
+
+import ca.sajid.custommultiworldplugin.CustomMultiWorldPlugin;
+import ca.sajid.custommultiworldplugin.Utils;
+import ca.sajid.custommultiworldplugin.util.BaseModule;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class RespawnWorld extends BaseModule implements Listener {
+
+    private final CustomMultiWorldPlugin plugin = CustomMultiWorldPlugin.getPlugin();
+
+    @Override
+    public void onEnable() {
+        getPlugin().getServer().getPluginManager().registerEvents(this, getPlugin());
+    }
+
+    @EventHandler
+    public void PlayerRespawnEvent(PlayerRespawnEvent e) {
+        Player p = e.getPlayer();
+        e.setRespawnLocation(p.getWorld().getSpawnLocation());
+    }
+}
