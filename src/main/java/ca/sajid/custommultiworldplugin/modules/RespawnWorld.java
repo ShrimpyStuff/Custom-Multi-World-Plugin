4ef402478dc30e42f8342efc0147435476f72580
ShrimpyStuff <52041627+ShrimpyStuff@users.noreply.github.com>
2021-02-14 21:00:22 -0500

    Update RespawnWorld.java

--- src/main/java/ca/sajid/custommultiworldplugin/modules/RespawnWorld.java
+++ src/main/java/ca/sajid/custommultiworldplugin/modules/RespawnWorld.java
@@ -6,6 +6,7 @@ import ca.sajid.custommultiworldplugin.util.BaseModule;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
 public class RespawnWorld extends BaseModule implements Listener {
 
